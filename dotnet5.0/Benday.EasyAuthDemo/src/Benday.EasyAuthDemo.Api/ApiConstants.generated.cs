75cdeae1d92644840a8152d432911e8d0fe0e0f7
Benjamin Day <benday@benday.com>
2020-11-25 10:32:19 -0500

    Fixed problems with one to many collection editors

---
+++ dotnet5.0/Benday.EasyAuthDemo/src/Benday.EasyAuthDemo.Api/ApiConstants.generated.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Benday.EasyAuthDemo.Api
+{
+    public static partial class ApiConstants
+    {
+        public const string DefaultAttributeStatus = "ACTIVE";
+        public const string StatusActive = "ACTIVE";
+        public const string StatusDeleted = "DELETED";
+        public const string StatusInactive = "INACTIVE";
+        public const int UnsavedId = 0;
+        public const string ClaimName_UserId = "Local.UserId";
+        public const string Username_GlobalUser = "GLOBAL_USER";
+    }
+}
