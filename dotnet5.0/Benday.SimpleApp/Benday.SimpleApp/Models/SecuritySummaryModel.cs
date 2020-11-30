c12f06829b311488a211bc04753b74005fcdf36a
Benjamin Day <benday@benday.com>
2020-11-30 14:25:02 -0500

    Added security summary to simple app

---
+++ dotnet5.0/Benday.SimpleApp/Benday.SimpleApp/Models/SecuritySummaryModel.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Benday.SimpleApp.Models
+{
+    public class SecuritySummaryModel
+    {
+        public bool IsClaimsPrincipalNull { get; set; }
+        public string Username { get; set; }
+        public IEnumerable<System.Security.Claims.Claim> Claims { get; set; }
+
+        public bool IsPrimaryIdentityNull { get; set; }
+        public string PrimaryIdentityAuthenticationType { get; set; }
+        public bool PrimaryIdentityIsAuthenticated { get; set; }
+        public string PrimaryIdentityName { get; set; }
+        public string IdentitiesInfo { get; set; }
+        public IEnumerable<ClaimsIdentity> Identities { get; set; }
+        public IHeaderDictionary Headers { get; set; }
+        public IRequestCookieCollection Cookies { get; set; }
+    }
+}
